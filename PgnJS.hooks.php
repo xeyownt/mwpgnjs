244d221f49e5f090107570e6ed90b178ac770d65
Michaël Peeters <xeyownt@users.noreply.github.com>
2017-12-30 13:41:25 +0100

    Apply some naming conventions

--- PgnJS.hooks.php
+++ PgnJS.hooks.php
@@ -27,112 +27,113 @@ class PgnJSHooks {
 }
 
 class PgnJS {
-    const STYLE          = "style";
-    const C_CLASS        = "class";
-    const MODE           = "mode";
-    const MODE_DEFAULTS  = "defaults";
-    const POSITION       = "position";
-    const SHOW_NOTATION  = "shownotation";  // Must be lower case or test fail!
-    const ORIENTATION    = "orientation";
-    const THEME          = "theme";
-    const PIECE_STYLE    = "piecestyle";    // Must be lower case or test fail!
-    const TIMER_TIME     = "timertime";     // Must be lower case or test fail!
-    const LOCALE         = "locale";
-    const BOARD_SIZE     = "boardsize";     // Must be lower case or test fail!
-    const MOVES_WIDTH    = "moveswidth";    // Must be lower case or test fail!
-    const MOVES_HEIGHT   = "movesheight";   // Must be lower case or test fail!
-    const SCROLLABLE     = "scrollable";
-    const LAYOUT         = "layout";
-    const GO_TO          = "goto";          // Should be GOTO but reserved word
+    const A_STYLE          = "style";
+    const A_CLASS          = "class";
+    const A_MODE           = "mode";
+    const MODE_DEFAULTS    = "defaults";
+    const A_POSITION       = "position";
+    const A_SHOW_NOTATION  = "shownotation";  // Must be lower case or test fail!
+    const A_ORIENTATION    = "orientation";
+    const A_THEME          = "theme";
+    const A_PIECE_STYLE    = "piecestyle";    // Must be lower case or test fail!
+    const A_TIMER_TIME     = "timertime";     // Must be lower case or test fail!
+    const A_LOCALE         = "locale";
+    const A_BOARD_SIZE     = "boardsize";     // Must be lower case or test fail!
+    const A_MOVES_WIDTH    = "moveswidth";    // Must be lower case or test fail!
+    const A_MOVES_HEIGHT   = "movesheight";   // Must be lower case or test fail!
+    const A_SCROLLABLE     = "scrollable";
+    const A_LAYOUT         = "layout";
+    const A_GOTO           = "goto";
+    const DEFAULTS         = array( 'style' => 'width: 240px', 'pieceStyle' => 'merida' );
     private static $board_id = 0;
     private static $board_defaults = array();
     private static $board_class_defaults = array();
 
     // Render <pgn>
     static public function renderPgnjs( $parser, $input, array $args ) {
-        $style        = isset($args[self::STYLE]) ? $args[self::STYLE] : null;
-        $class        = isset($args[self::C_CLASS]) ? $args[self::C_CLASS] : null;
-        $mode         = isset($args[self::MODE]) ? $args[self::MODE] : "view";
-        $position     = isset($args[self::POSITION]) ? $args[self::POSITION] : null;
-        $showNotation = isset($args[self::SHOW_NOTATION]) ? $args[self::SHOW_NOTATION] : null;
-        $orientation  = isset($args[self::ORIENTATION]) ? $args[self::ORIENTATION] : null;
-        $theme        = isset($args[self::THEME]) ? $args[self::THEME] : null;
-        $pieceStyle   = isset($args[self::PIECE_STYLE]) ? $args[self::PIECE_STYLE] : null;
-        $timerTime    = isset($args[self::TIMER_TIME]) ? $args[self::TIMER_TIME] : null;
-        $locale       = isset($args[self::LOCALE]) ? $args[self::LOCALE] : null;
-        $boardSize    = isset($args[self::BOARD_SIZE]) ? $args[self::BOARD_SIZE] : null;
-        $movesWidth   = isset($args[self::MOVES_WIDTH]) ? $args[self::MOVES_WIDTH] : null;
-        $movesHeight  = isset($args[self::MOVES_HEIGHT]) ? $args[self::MOVES_HEIGHT] : null;
-        $scrollable   = isset($args[self::SCROLLABLE]) ? $args[self::SCROLLABLE] : null;
-        $layout       = isset($args[self::LAYOUT]) ? $args[self::LAYOUT] : null;
-        $goto         = isset($args[self::GO_TO]) ? $args[self::GO_TO] : null;
+        $a_style        = isset($args[self::A_STYLE]) ? $args[self::A_STYLE] : null;
+        $a_class        = isset($args[self::A_CLASS]) ? $args[self::A_CLASS] : null;
+        $a_mode         = isset($args[self::A_MODE]) ? $args[self::A_MODE] : "view";
+        $a_position     = isset($args[self::A_POSITION]) ? $args[self::A_POSITION] : null;
+        $a_showNotation = isset($args[self::A_SHOW_NOTATION]) ? $args[self::A_SHOW_NOTATION] : null;
+        $a_orientation  = isset($args[self::A_ORIENTATION]) ? $args[self::A_ORIENTATION] : null;
+        $a_theme        = isset($args[self::A_THEME]) ? $args[self::A_THEME] : null;
+        $a_pieceStyle   = isset($args[self::A_PIECE_STYLE]) ? $args[self::A_PIECE_STYLE] : null;
+        $a_timerTime    = isset($args[self::A_TIMER_TIME]) ? $args[self::A_TIMER_TIME] : null;
+        $a_locale       = isset($args[self::A_LOCALE]) ? $args[self::A_LOCALE] : null;
+        $a_boardSize    = isset($args[self::A_BOARD_SIZE]) ? $args[self::A_BOARD_SIZE] : null;
+        $a_movesWidth   = isset($args[self::A_MOVES_WIDTH]) ? $args[self::A_MOVES_WIDTH] : null;
+        $a_movesHeight  = isset($args[self::A_MOVES_HEIGHT]) ? $args[self::A_MOVES_HEIGHT] : null;
+        $a_scrollable   = isset($args[self::A_SCROLLABLE]) ? $args[self::A_SCROLLABLE] : null;
+        $a_layout       = isset($args[self::A_LAYOUT]) ? $args[self::A_LAYOUT] : null;
+        $a_goto         = isset($args[self::A_GOTO]) ? $args[self::A_GOTO] : null;
 
         $board = array();
-        if( $mode !== self::MODE_DEFAULTS) {
-            foreach( explode(' ', $class) as $c ) {
+        if( $a_mode !== self::MODE_DEFAULTS) {
+            foreach( explode(' ', $a_class) as $c ) {
                 if( $c and isset(self::$board_class_defaults[$c]) ) {
                     $board += self::$board_class_defaults[$c];  // First listed class as precedence
                 }
             }
             $board += self::$board_defaults;  // non-class defaults have less precedence
-            $board += array( 'style' => 'width: 240px', 'pieceStyle' => 'merida' );  // Finally hard-coded defaults
+            $board += self::DEFAULTS;         // Finally hard-coded defaults have least precedence
         }
         $id = "pgnjs".(++self::$board_id);
 
         if( $input ) {
             $board['pgn'] = $input;
         } else {
-            if( ($mode !== 'edit') and ($mode !== self::MODE_DEFAULTS) ) {
-                $mode = 'board';         // Fall-back to 'board' mode, unless in edit or defaults mode
+            if( ($a_mode !== 'edit') and ($a_mode !== self::MODE_DEFAULTS) ) {
+                $a_mode = 'board';         // Fall-back to 'board' mode, unless in edit or defaults mode
             }
         }
-        if( $style ) {
-            $board['style'] = $style;
+        if( $a_style ) {
+            $board['style'] = $a_style;
         }
-        if( $position ) {
-            $board['position'] = $position;
+        if( $a_position ) {
+            $board['position'] = $a_position;
         }
-        if( $showNotation ) {
-            $board['showNotation'] = ($showNotation === 'true');
+        if( $a_showNotation ) {
+            $board['showNotation'] = ($a_showNotation === 'true');
         }
-        if( $orientation ) {
-            $board['orientation'] = $orientation;
+        if( $a_orientation ) {
+            $board['orientation'] = $a_orientation;
         }
-        if( $theme ) {
-            $board['theme'] = $theme;
+        if( $a_theme ) {
+            $board['theme'] = $a_theme;
         }
-        if( $pieceStyle ) {
-            $board['pieceStyle'] = $pieceStyle;
+        if( $a_pieceStyle ) {
+            $board['pieceStyle'] = $a_pieceStyle;
         }
-        if( $timerTime ) {
-            $board['timerTime'] = $timerTime;
+        if( $a_timerTime ) {
+            $board['timerTime'] = $a_timerTime;
         }
-        if( $locale ) {
-            $board['locale'] = $locale;
+        if( $a_locale ) {
+            $board['locale'] = $a_locale;
         }
-        if( $boardSize ) {
-            $board['boardSize'] = $boardSize;
+        if( $a_boardSize ) {
+            $board['boardSize'] = $a_boardSize;
         }
-        if( $movesWidth ) {
-            $board['movesWidth'] = $movesWidth;
+        if( $a_movesWidth ) {
+            $board['movesWidth'] = $a_movesWidth;
         }
-        if( $movesHeight ) {
-            $board['movesHeight'] = $movesHeight;
+        if( $a_movesHeight ) {
+            $board['movesHeight'] = $a_movesHeight;
         }
-        if( $scrollable ) {
-            $board['scrollable'] = ($scrollable === 'true');
+        if( $a_scrollable ) {
+            $board['scrollable'] = ($a_scrollable === 'true');
         }
-        if( $layout ) {
-            $board['layout'] = $layout;
+        if( $a_layout ) {
+            $board['layout'] = $a_layout;
         }
-        if( $goto && ($goto === 'last' or $goto === 'first') ) {
-            $board['goto'] = $goto;
+        if( $a_goto && ($a_goto === 'last' or $a_goto === 'first') ) {
+            $board['goto'] = $a_goto;
         }
 
-        if( $mode === self::MODE_DEFAULTS ) {
+        if( $a_mode === self::MODE_DEFAULTS ) {
             // If mode is defaults, we save the config as defaults
             $has_class = false;
-            foreach( explode(' ', $class) as $c ) {
+            foreach( explode(' ', $a_class) as $c ) {
                 if( $c ) {
                     self::$board_class_defaults[$c] = $board;
                     $has_class = true;
@@ -145,17 +146,17 @@ class PgnJS {
             return "";
         } else {
             // mode is not setting defaults, generate HTML and JS to display the board
-            $board['mode'] = $mode;                     // must set the mode for our parser
+            $board['mode'] = $a_mode;                     // must set the mode for our parser
 
             // Remove attributes unknown to PgnViewerJS
-            $style = $board['style'];
+            $a_style = $board['style'];
             unset($board['style']);
 
             $jsBoard = json_encode($board);
             $script = "<script>window.PgnJSBoards = window.PgnJSBoards || {}; window.PgnJSBoards.$id = $jsBoard;</script>";
 
-            $class_attr = $class ? " class=\"$class\"" : "";
-            return "<div id=\"$id\"$class_attr style=\"$style\"></div>$script";
+            $class_attr = $a_class ? " class=\"$a_class\"" : "";
+            return "<div id=\"$id\"$class_attr style=\"$a_style\"></div>$script";
         }
     }
 }
